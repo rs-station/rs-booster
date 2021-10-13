ad54ea623ea168f77c62fb45490290724e72254c
Jack Greisman <Jack.Greisman@gmail.com>
2021-10-13 15:13:59 -0400

    Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="efxtools",
+    version="0.0.1",
+    author="Jack B. Greisman",
+    author_email="greisman@g.harvard.edu",
+    packages=find_packages(),
+    description="",
+    install_requires=[
+        "reciprocalspaceship",
+    ],
+)
