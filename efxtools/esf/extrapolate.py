0fa87c316060c83d2e6bc4e6086e302710f1d7a0
Jack Greisman <Jack.Greisman@gmail.com>
2022-03-15 17:40:13 -0400

    Update efxtools.extrapolate to support SigF_ref

--- efxtools/esf/extrapolate.py
+++ efxtools/esf/extrapolate.py
@@ -5,8 +5,15 @@ Make extrapolated structure factors for refinement.
 Equations
 ---------
 
+with reference:
+
+    F_{esf}    = f * (F_{on} - F_{off}) + F_{ref}
+    SigF_{esf} = sqrt( ( (f**2)*(SigF_{on}**2) ) + ( (f**2)*(SigF_{off}**2) ) + (SigF_{ref}**2))
+
+with calc:
+
     F_{esf}    = f * (F_{on} - F_{off}) + F_{calc}
-    SigF_{esf} = sqrt( ( (f**2)*(SigF_{on}**2) ) + ( ((f**2)*(SigF_{off}**2) ) )
+    SigF_{esf} = sqrt( ( (f**2)*(SigF_{on}**2) ) + ( (f**2)*(SigF_{off}**2) ) )
 
 where f, is the extrapolation factor.
 
@@ -22,48 +29,94 @@ import numpy as np
 
 import reciprocalspaceship as rs
 
+
 def parse_arguments():
     """Parse commandline arguments"""
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
-                                     description=__doc__)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter, description=__doc__
+    )
 
     # Required arguments
-    parser.add_argument("on_mtz", help="MTZ containing ON data")
-    parser.add_argument("F_on", help="Column for F_on data in on_mtz")
-    parser.add_argument("SigF_on", help="Column for SigF_on data in on_mtz")
-    parser.add_argument("off_mtz", help="MTZ containing OFF data")
-    parser.add_argument("F_off", help="Column for F_off data in off_mtz")
-    parser.add_argument("SigF_off", help="Column for SigF_off data in off_mtz")
-    parser.add_argument("calc_mtz", help="MTZ containing CALC data")
-    parser.add_argument("F_calc", help="Column for F_calc data in calc_mtz")
+    parser.add_argument(
+        "-on",
+        "--onmtz",
+        nargs=3,
+        metavar=("mtz", "f_col", "sig_col"),
+        required=True,
+        help="MTZ to be used as `on` data. Specified as (filename, F, SigF)",
+    )
+    parser.add_argument(
+        "-off",
+        "--offmtz",
+        nargs=3,
+        metavar=("mtz", "data_col", "sig_col"),
+        required=True,
+        help=("MTZ to be used as `off` data. Specified as (filename, F, SigF)"),
+    )
+
+    # One of these must provided
+    parser.add_argument(
+        "-calc",
+        "--calcmtz",
+        nargs=2,
+        metavar=("mtz", "data_col"),
+        help=("MTZ to be used as `calc` data. Specified as (filename, F, SigF)"),
+    )
+    parser.add_argument(
+        "-ref",
+        "--refmtz",
+        nargs=3,
+        metavar=("mtz", "data_col", "sig_col"),
+        help=("MTZ to be used as `ref` data. Specified as (filename, F, SigF)"),
+    )
 
     # Optional arguments
-    parser.add_argument("-f", "--factor", type=float, default=10.0,
-                        help="Extrapolation factor")
-    parser.add_argument("-o", "--outfile", default="esf.mtz",
-                        help="Output MTZ filename")
+    parser.add_argument(
+        "-f", "--factor", type=float, default=10.0, help="Extrapolation factor"
+    )
+    parser.add_argument(
+        "-o", "--outfile", default="esf.mtz", help="Output MTZ filename"
+    )
 
     return parser.parse_args()
+
 
 def main():
 
     # Parse commandline arguments
     args = parse_arguments()
+    on, f_on, sigf_on = args.onmtz
+    off, f_off, sigf_off = args.offmtz
+
+    if args.calcmtz and args.refmtz:
+        raise ValueError("Only specify `-calc` or `-ref`, not both.")
+    elif args.calcmtz:
+        calc, f_calc = args.calcmtz
+        sigf_calc = None
+    elif args.refmtz:
+        calc, f_calc, sigf_calc = args.refmtz
+    else:
+        raise ValueError("One of `-calc` or `-ref` must be specified.")
 
     # Read MTZ files
-    on   = rs.read_mtz(args.on_mtz)
-    off  = rs.read_mtz(args.off_mtz)
-    calc = rs.read_mtz(args.calc_mtz)
+    on = rs.read_mtz(on)
+    off = rs.read_mtz(off)
+    calc = rs.read_mtz(calc)
 
     # Canonicalize column names
-    on.rename(columns={args.F_on:"F", args.SigF_on:"SigF"}, inplace=True)
-    off.rename(columns={args.F_off:"F", args.SigF_off:"SigF"}, inplace=True)
-    calc.rename(columns={args.F_calc:"F_calc"}, inplace=True)
+    on.rename(columns={f_on: "F", sigf_on: "SigF"}, inplace=True)
+    off.rename(columns={f_off: "F", sigf_off: "SigF"}, inplace=True)
+    calc.rename(columns={f_calc: "F_calc"}, inplace=True)
+
+    if sigf_calc:
+        calc.rename(columns={sigf_calc: "SigF_calc"}, inplace=True)
+        calc = calc[["F_calc", "SigF_calc"]]
+    else:
+        calc = calc[["F_calc"]]
 
     # Subset DataSet objects to relevant columns
-    on   = on[["F", "SigF"]]
-    off  = off[["F", "SigF"]]
-    calc = calc[["F_calc"]] 
+    on = on[["F", "SigF"]]
+    off = off[["F", "SigF"]]
 
     # Merge into common DataSet, keeping cell/spacegroup from on data
     joined = on.merge(off, on=["H", "K", "L"], suffixes=("_on", "_off"))
@@ -71,15 +124,27 @@ def main():
 
     # Compute F_esf and SigF_esf
     factor = args.factor
-    joined["F_esf"] = factor*(joined["F_on"] - joined["F_off"]) + joined["F_calc"]
+    joined["F_esf"] = factor * (joined["F_on"] - joined["F_off"]) + joined["F_calc"]
     if np.array_equal(joined["F_off"].to_numpy(), joined["F_calc"].to_numpy()):
         print("F_off == F_calc... changing error propagation accordingly.")
-        joined["SigF_esf"] = np.sqrt(((factor**2)*(joined["SigF_on"]**2)) + (((factor-1)**2)*(joined["SigF_off"]**2)))
+        joined["SigF_esf"] = np.sqrt(
+            ((factor ** 2) * (joined["SigF_on"] ** 2))
+            + (((factor - 1) ** 2) * (joined["SigF_off"] ** 2))
+        )
     else:
-        joined["SigF_esf"] = np.sqrt(((factor**2)*(joined["SigF_on"]**2)) + ((factor**2)*(joined["SigF_off"]**2)))
+        joined["SigF_esf"] = np.sqrt(
+            ((factor ** 2) * (joined["SigF_on"] ** 2))
+            + ((factor ** 2) * (joined["SigF_off"] ** 2))
+        )
+
+    if sigf_calc:
+        joined["SigF_esf"] = np.sqrt(
+            (joined["SigF_esf"] ** 2) + (joined["SigF_calc"] ** 2)
+        )
 
     joined.infer_mtz_dtypes(inplace=True)
     joined.write_mtz(args.outfile)
+
 
 if __name__ == "__main__":
     main()
