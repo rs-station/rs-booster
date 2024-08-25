db6db099072d2f5b4aa1760f192fe7e6053c3336
Derek Mendez <dermen@lbl.gov>
2024-08-25 00:54:01 -0500

    addresses review

--- rsbooster/io/dials2mtz.py
+++ rsbooster/io/dials2mtz.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+import logging
+import glob
+
+
 from reciprocalspaceship.io import dials
 
 
@@ -20,24 +24,56 @@ def get_parser():
         help="unit cell params (default will be average experiment crystal)",
     )
     parser.add_argument("--symbol", type=str, default=None)
+    parser.add_argument("--verbose", action="store_true", help="show some stdout")
     return parser
 
 
 def _write(ds, mtzname):
     """write the RS dataset to mtz file"""
     ds.write_mtz(mtzname)
-    print("Wrote %s." % mtzname)
-    print("Done!")
+
+
+def get_fnames(dirnames, verbose=False):
+    fnames = []
+    for dirname in dirnames:
+        fnames += glob.glob(dirname + "/*integrated.refl")
+    if verbose:
+        print("Found %d files" % len(fnames))
+    return fnames
+
+
+def _set_logger(verbose, silence_ray=True):
+    logger = logging.getLogger("rs.io.dials")
+    console = logger.handlers[0]
+    if verbose:
+        logger.setLevel(logging.DEBUG)
+        console.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.CRITICAL)
+        console.setLevel(logging.CRITICAL)
+
+    if silence_ray:
+        ray_level = logging.CRITICAL
+    else:
+        ray_level = logging.DEBUG
+
+    for _ray_name in ("ray", "ray.remote"):
+        ray_logger = logging.getLogger(_ray_name)
+        ray_logger.setLevel(ray_level)
+        for handler in ray_logger.handlers:
+            handler.setLevel(ray_level)
 
 
 def ray_main():
     parser = get_parser()
-    parser.add_argument("--nj", default=10, type=int, help="number of workers!")
+    parser.add_argument("--numjobs", default=10, type=int, help="number of workers!")
     args = parser.parse_args()
     assert args.ucell is not None
     assert args.symbol is not None
+    _set_logger(args.verbose, silence_ray=not args.verbose)
 
-    ds = dials.read_dials_stills(args.dirnames, ucell=args.ucell, symbol=args.symbol, nj=args.nj)
+    fnames = get_fnames(args.dirnames, args.verbose)
+    ds = dials.read_dials_stills(fnames, unitcell=args.ucell, spacegroup=args.symbol, numjobs=args.numjobs)
     _write(ds, args.mtz)
 
 
@@ -49,7 +85,9 @@ def mpi_main():
     from mpi4py import MPI
     COMM = MPI.COMM_WORLD
     from reciprocalspaceship.io.dials_mpi import read_dials_stills_mpi
-    ds = read_dials_stills_mpi(args.dirnames, ucell=args.ucell, symbol=args.symbol)
+    _set_logger(args.verbose)
+    fnames = get_fnames(args.dirnames, args.verbose)
+    ds = read_dials_stills_mpi(fnames, unitcell=args.ucell, spacegroup=args.symbol)
     if COMM.rank==0:
         _write(ds, args.mtz)
 
